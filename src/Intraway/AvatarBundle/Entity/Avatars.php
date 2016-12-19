bc53592f728018e4ea8b0f04776b3c43e34578f8
Deivid <Deivid@Deivid-PC>
2016-12-19 02:03:46 -0500

    Create Entity Avatars

---
+++ src/Intraway/AvatarBundle/Entity/Avatars.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Intraway\AvatarBundle\Entity;
+
+/**
+ * Avatars
+ */
+class Avatars
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @var string
+     */
+    private $mimetype;
+
+    /**
+     * @var integer
+     */
+    private $size;
+
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return Avatars
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Avatars
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set mimetype
+     *
+     * @param string $mimetype
+     *
+     * @return Avatars
+     */
+    public function setMimetype($mimetype)
+    {
+        $this->mimetype = $mimetype;
+
+        return $this;
+    }
+
+    /**
+     * Get mimetype
+     *
+     * @return string
+     */
+    public function getMimetype()
+    {
+        return $this->mimetype;
+    }
+
+    /**
+     * Set size
+     *
+     * @param integer $size
+     *
+     * @return Avatars
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * Get size
+     *
+     * @return integer
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+}
+
