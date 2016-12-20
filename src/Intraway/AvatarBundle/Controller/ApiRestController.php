09e8cedb1b2bf29d7e13d18878e4a77b009c8bda
Deivid <Deivid@Deivid-PC>
2016-12-19 22:43:35 -0500

    Create method uploadAction to upload image

--- src/Intraway/AvatarBundle/Controller/ApiRestController.php
+++ src/Intraway/AvatarBundle/Controller/ApiRestController.php
@@ -4,6 +4,7 @@ namespace Intraway\AvatarBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ApiRestController extends FOSRestController {
@@ -13,6 +14,22 @@ class ApiRestController extends FOSRestController {
         $view = View::create();
         $view->setData(array("id" => $id, "nombre" => "cristian"));
         return $this->handleView($view);
+    }
+
+    public function uploadAction(Request $request) {
+        $file = $request->files->get('uploadFiles');
+        $imgLashes = addslashes($file);
+        $imgFileContents = file_get_contents($imgLashes);
+        $imgEncode = base64_encode($imgFileContents);
+
+        $view = View::create();
+        $view->setData(array(
+            "nombre" => "cristian"));
+        return $this->handleView($view);
+    }
+    
+    private function safeImg(){
+        
     }
 
 }
