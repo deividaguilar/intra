fa4c78794f3ce946212031b0b7b9349f1b195aff
Deivid <Deivid@Deivid-PC>
2016-12-22 01:54:54 -0500

    Create object to load all images. Create event click to button search create event click to button save Create function to valid email setup thumbnails and imaged load.

--- src/Intraway/AvatarBundle/Controller/ApiRestController.php
+++ src/Intraway/AvatarBundle/Controller/ApiRestController.php
@@ -6,6 +6,8 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Intraway\AvatarBundle\Entity\Avatars;
+use Intraway\AvatarBundle\Entity\Emails;
+use Symfony\Component\Filesystem\Filesystem;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ApiRestController extends FOSRestController {
@@ -15,10 +17,13 @@ class ApiRestController extends FOSRestController {
     private $imgMimeType;
     private $imgSize;
     private $imgExten;
+    private $imgThumb;
+    private $request;
 
-    public function indexAction($opt) {
+    public function indexAction($opt, Request $request) {
+
 //        return $this->render('IntrawayAvatarBundle:Default:index.html.twig');
-
+        $this->setRequest($request);
         $view = View::create();
         $view->setData($this->selectOption($opt));
         return $this->handleView($view);
@@ -30,10 +35,58 @@ class ApiRestController extends FOSRestController {
                 $data = $this->showAvatares();
                 break;
             case 2 :
-                $data = "";
+                $data = $this->searchAvatar();
+                break;
+            case 3 :
+                $data = $this->saveData();
                 break;
         }
         return $data;
+    }
+
+    private function searchAvatar() {
+
+        $email = $this->getDoctrine()
+                        ->getRepository("IntrawayAvatarBundle:Emails")->find($this->getRequest()->get('email'));
+
+        if (empty($email)) {
+            return array("message" => "Data not found. You can save your data!!!", "img" => $this->imgDefault()->getId());
+        } else {
+            return array("message" => "Data found", "img" => $email->getIdavatar()->getId());
+        }
+//        return $this->insertEmail($avatar, $this->getRequest()->get('email'), $email);
+    }
+
+    private function saveData() {
+        $email = $this->getDoctrine()
+                        ->getRepository("IntrawayAvatarBundle:Emails")->find($this->getRequest()->get('email'));
+        $avatar = $this->getDoctrine()
+                        ->getRepository("IntrawayAvatarBundle:Avatars")->find($this->getRequest()->get('idAvatar'));
+
+        if (empty($email)) {
+            $emailIn = '';
+            $email = new Emails();
+            $email->setHashmd5($this->getRequest()->get('email'));
+        }
+
+        $finalAvatar = empty($avatar) ? $this->imgDefault() : $avatar;
+        $email->setIdavatar($finalAvatar);
+        try {
+            $em = $this->getDoctrine()->getManager();
+            if (empty($emailIn)) {
+                $em->persist($email);
+            }
+            $em->flush();
+        } catch (Exception $ex) {
+            return array("message" => "Error saved image", "img" => $this->imgDefault()->getId());
+        }
+        return array("message" => "Image saved correctly", "img" => $finalAvatar->getId());
+    }
+
+    private function imgDefault() {
+        $avatar = $this->getDoctrine()
+                        ->getRepository("IntrawayAvatarBundle:Avatars")->findBy(array("name" => 'empty.jpg'));
+        return $avatar[0];
     }
 
     private function showAvatares() {
@@ -45,15 +98,16 @@ class ApiRestController extends FOSRestController {
 
     private function createObjAvatar($rows) {
         $salida = array();
-        foreach ($rows as $objAvatar) {
+        foreach ($rows as $arrAvatar) {
             $avatar = [];
-            $avatar['image'] = stream_get_contents($objAvatar->getImage());
-            $avatar['mimetype'] = $objAvatar->getMimetype();
-            $avatar['size'] = $objAvatar->getSize();
-            $avatar['sizefile'] = $objAvatar->getSizefile();
-            $avatar['extension'] = $objAvatar->getExtension();
-            $avatar['name'] = $objAvatar->getName();
-            $salida[$objAvatar->getId()] = $avatar;
+            $avatar['image'] = stream_get_contents($arrAvatar->getImage());
+            $avatar['mimetype'] = $arrAvatar->getMimetype();
+            $avatar['size'] = $arrAvatar->getSize();
+            $avatar['sizefile'] = $arrAvatar->getSizefile();
+            $avatar['extension'] = $arrAvatar->getExtension();
+            $avatar['thumb'] = stream_get_contents($arrAvatar->getThumb());
+            $avatar['name'] = $arrAvatar->getName();
+            $salida[$arrAvatar->getId()] = $avatar;
         }
         return $salida;
     }
@@ -67,11 +121,41 @@ class ApiRestController extends FOSRestController {
         $this->setImgName($file->getClientOriginalName());
         $this->setImgSize($file->getClientSize());
         $this->setImgExten($file->getClientOriginalExtension());
+        $this->setImgThumb($this->createThumImg($file));
 
         $view = View::create();
         $view->setData(array(
             "message" => $this->validateImg()));
         return $this->handleView($view);
+    }
+
+    private function createThumImg($file) {
+
+        $percent = 0.5;
+        list($width, $height) = getimagesize($file);
+        $newwidth = $width * $percent;
+        $newheight = $height * $percent;
+
+
+// Load
+        $thumb = imagecreatetruecolor($newwidth, $newheight);
+
+        $source = imagecreatefromstring(base64_decode($this->getImgEncode()));
+
+// Resize
+        imagecopyresampled($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+        imagejpeg($thumb, $this->getImgName());
+        $ds = DIRECTORY_SEPARATOR;
+
+        $tmpFile = str_replace("src{$ds}Intraway{$ds}AvatarBundle{$ds}Controller", "web" . $ds . $this->getImgName(), __DIR__);
+
+        $fs = new Filesystem();
+
+
+        $imgFileContents = file_get_contents($tmpFile);
+        $fs->remove($tmpFile);
+        return base64_encode($imgFileContents);
     }
 
     private function validateImg() {
@@ -91,6 +175,7 @@ class ApiRestController extends FOSRestController {
         $avatar->setSize(100);
         $avatar->setSizefile($this->getImgSize());
         $avatar->setName($this->getImgName());
+        $avatar->setThumb($this->getImgThumb());
         try {
             $em = $this->getDoctrine()->getManager();
             $em->persist($avatar);
@@ -146,6 +231,22 @@ class ApiRestController extends FOSRestController {
 
     private function setImgExten($exten) {
         $this->imgExten = $exten;
+    }
+
+    private function getImgThumb() {
+        return $this->imgThumb;
+    }
+
+    private function setImgThumb($thumb) {
+        $this->imgThumb = $thumb;
+    }
+
+    private function getRequest() {
+        return $this->request;
+    }
+
+    private function setRequest($request) {
+        $this->request = $request;
     }
 
 }
